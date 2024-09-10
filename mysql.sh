3c2fc0894f8e15e7b61fc8dce5c81900b1c4da99
rishikesh <malleswaraws@gmail.com>
2024-09-10 16:29:35 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -23,7 +23,7 @@ systemctl start mysqld &>>$LOGFILE
 mysql -h db.malleswariaws.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass -p${mysql_root_password} &>>$LOGFILE
     #VALIDATE $? "MYSQL Root password Setup"
 else
     echo -e "MYSQL Root password is already setup...$Y SKIPPING $N"

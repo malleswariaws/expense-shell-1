f106bc47e46cdff94a4bb63044b8a07c0bdb7321
rishikesh <malleswaraws@gmail.com>
2024-09-10 16:15:59 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -7,7 +7,7 @@ check_root
 echo "please enter DB password:"
 read -s mysql_root_password
 
-dnf install mysql-serverdh -y &>>$LOGFILE
+dnf install mysql-serverd -y &>>$LOGFILE
 #VALIDATE $? "Installing MYSQL Server"
 
 systemctl enable mysqld &>>$LOGFILE

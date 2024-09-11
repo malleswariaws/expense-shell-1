a65cc97ba01d6adc36904a454d78765499ade08c
rishikesh <malleswaraws@gmail.com>
2024-09-11 11:06:50 +0530

    functions

--- mysql.sh
+++ mysql.sh
@@ -4,7 +4,7 @@ source ./common.sh
 
 check_root
 
-echo "please enter DB password:"
+echo "please enter DB password:
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
@@ -20,7 +20,7 @@ systemctl start mysqld &>>$LOGFILE
 # VALIDATE $? "Setting up root password"
 
 #Below code will be useful for idempotent nature
-mysql -h db.malleswariaws.online -uroot -p${mysql_root_password} -e 'SHOW DATABASES;' &>>$LOGFILE
+mysql -h db.malleswariaws.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
